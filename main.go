3f91c96d9b1a1997dc0ddce93f439135f5cf6a5e
github-muzilong <712120393@qq.com>
2022-10-25 17:26:41 +0800

    添加启动日志

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"flag"
 
-	"go.uber.org/zap"
-
 	"github.com/github-muzilong/go-gin-example/bootstrap"
-	"github.com/github-muzilong/go-gin-example/pkg/g"
 )
 
 var configFile string
@@ -15,5 +12,4 @@ func main() {
 	flag.StringVar(&configFile, "config", "config/config.toml", "the config file")
 	flag.Parse()
 	bootstrap.Run(configFile)
-	g.Logger().Info("start http server", zap.Int("port", g.Config().App.Port))
 }
--- route/route.go
+++ route/route.go
@@ -6,6 +6,7 @@ import (
 
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 
 	"github.com/github-muzilong/go-gin-example/pkg/g"
 )
@@ -20,13 +21,17 @@ func Run() {
 	app := gin.New()
 	app.Use(ginzap.Ginzap(g.Logger(), time.RFC3339, true))
 	app.Use(ginzap.RecoveryWithZap(g.Logger(), true))
-	err := app.Run(fmt.Sprintf(":%d", g.Config().App.Port))
-	if err != nil {
-		panic(err)
-	}
+
+	g.Logger().Info("start http server", zap.Int("port", g.Config().App.Port))
+
 	// 认证路由
 	authRoute(app)
 	// 用户路由
 	userRouter(app)
 
+	// 启动http服务
+	err := app.Run(fmt.Sprintf(":%d", g.Config().App.Port))
+	if err != nil {
+		panic(err)
+	}
 }
